d169f6043c2764e4bbc34622e6ad1c7e3bef044e
Huang Shewei <huangshewei@gmail.com>
2022-05-31 15:17:56 -0400

    Import and exports

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-const person = {
-  firstName: 'yoshi',
-  age: 30,
-  favColor: 'green'
-}
-
-// const firstName = person.firstName
-// const favColor = person.favColor
-
-// We can do it like
-const { firstName, favColor, age  } = person
-console.log(firstName, favColor, age)
-
-
-const nums = [7, 9, 3, 5, 2]
-const [a, b, c] = nums
-console.log(a, b, c)
+// Import export exercise
+import { greet } from './ninjas.js'
+// import { ninjas } from './ninjas.js'
+import abc from './ninjas.js'
 
 
 
+// // Destructuring exercise
+// const person = {
+//   firstName: 'yoshi',
+//   age: 30,
+//   favColor: 'green'
+// }
 
-const getCalculations = (a, b) => {
+// // const firstName = person.firstName
+// // const favColor = person.favColor
 
-  // return {
-  //   sum: a + b,
-  //   diff: Math.abs(a - b),
-  //   product: a * b,
-  // }
-  // const { sum, product, diff} = getCalculations(7,9)
-
-  return [
-    a + b,
-    Math.abs(a - b),
-    a * b
-  ]
-}
-
-const [ sum, diff, product ] = getCalculations(7, 9)
-console.log(sum, product, diff)
+// // We can do it like
+// const { firstName, favColor, age  } = person
+// console.log(firstName, favColor, age)
 
 
-const doSomething = ({ name }) => {
-  console.log(name)
-}
-doSomething({ name: 'shaun', age: 35 })
+// const nums = [7, 9, 3, 5, 2]
+// const [a, b, c] = nums
+// console.log(a, b, c)
+
+
+
+
+// const getCalculations = (a, b) => {
+
+//   // return {
+//   //   sum: a + b,
+//   //   diff: Math.abs(a - b),
+//   //   product: a * b,
+//   // }
+//   // const { sum, product, diff} = getCalculations(7,9)
+
+//   return [
+//     a + b,
+//     Math.abs(a - b),
+//     a * b
+//   ]
+// }
+
+// const [ sum, diff, product ] = getCalculations(7, 9)
+// console.log(sum, product, diff)
+
+
+// const doSomething = ({ name }) => {
+//   console.log(name)
+// }
+// doSomething({ name: 'shaun', age: 35 })
+
+
+console.log(abc)
+greet()
